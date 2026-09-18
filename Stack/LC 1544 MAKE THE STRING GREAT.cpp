d327fae4f1397e9f21b26b66bac0aea2551a2eb4
Sreeganesh <sreeganeshsrinivasan51@gmail.com>
2026-09-18 23:29:41 +0530

    Create LC 1544 MAKE THE STRING GREAT.cpp
    A good start to The data structure "STACKS"

---
+++ Stack/LC 1544 MAKE THE STRING GREAT.cpp
@@ -0,0 +1,15 @@
+char* makeGood(char* s) {
+    int top = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (top > 0 && abs(s[i] - s[top-1]) == 32) {
+            top--; 
+        } else {
+            s[top] = s[i];
+            top++; 
+        }
+    }
+    s[top] = '\0';
+    return s; 
+}
+
